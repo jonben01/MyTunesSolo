26af3e9d66ec59ab9342ef5efc3eef733512523c
Jonas Bendorff <jonben01@easv365.dk>
2024-11-28 16:10:06 +0100

    Created filechooser method with filters

--- src/main/java/dk/easv/Jonas_MyTunesSolo/GUI/Controller/NewSongController.java
+++ src/main/java/dk/easv/Jonas_MyTunesSolo/GUI/Controller/NewSongController.java
@@ -1,12 +1,30 @@
 package dk.easv.Jonas_MyTunesSolo.GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class NewSongController {
+    @FXML
+    public TextField txtFilePath;
+
     public void btnHandleMenuAddSong(ActionEvent actionEvent) {
         
     }
 
     public void btnHandleFileChooser(ActionEvent actionEvent) {
+        //create file chooser object, with filters for relevant file types.
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select audio file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3"));
+        //open file chooser dialog window
+        File file = fileChooser.showOpenDialog(null);
+        //if file exists update text field
+        if (file != null) {
+            txtFilePath.setText(file.getAbsolutePath());
+        }
     }
 }
