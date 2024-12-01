dd0eec84f1018881655fcac984daae395e23a97e
Jonas Bendorff <jonben01@easv365.dk>
2024-12-01 22:08:51 +0100

    User gets alert if file path is empty, added some comments

--- src/main/java/dk/easv/Jonas_MyTunesSolo/GUI/Controller/NewSongController.java
+++ src/main/java/dk/easv/Jonas_MyTunesSolo/GUI/Controller/NewSongController.java
@@ -6,8 +6,6 @@ import dk.easv.Jonas_MyTunesSolo.BE.Song;
 import dk.easv.Jonas_MyTunesSolo.GUI.GenreModel;
 import dk.easv.Jonas_MyTunesSolo.GUI.SongModel;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -59,18 +57,29 @@ public class NewSongController implements Initializable {
             Genre selectedGenre = (Genre) this.cmbGenre.getSelectionModel().getSelectedItem();
             int duration = Integer.parseInt(txtDuration.getText());
 
+            if (txtFilePath.getText() == null || txtFilePath.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Please select a file");
+                alert.showAndWait();
+                return;
+            }
+
             String songDestinationDir = "src/main/resources/Songs";
             Path songDestinationPath = Paths.get(songDestinationDir, new File(txtFilePath.getText()).getName());
 
             try {
                 Files.copy(Paths.get(txtFilePath.getText()), songDestinationPath);
             } catch (FileAlreadyExistsException e) {
+                //created an alert if a FileAlreadyExistsException has been caught.
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("FileAlreadyExistsException");
                 alert.setHeaderText("File already exists");
-                alert.setContentText("Replace existing song with new song?");
+                alert.setContentText("Replace existing file with new file?");
 
                 Optional<ButtonType> result = alert.showAndWait();
+                //if you click okay, it will override the currently existing file.
                 if (result.get() == ButtonType.OK) {
                     //get songToBeDeleted if a song with the file path of the destination path exists
                     Song songToBeDeleted = songModel.getSongByFilePath(songDestinationPath.toString());
@@ -91,19 +100,18 @@ public class NewSongController implements Initializable {
             //TODO FIGURE OUT HOW TO GET DURATION IN A GOOD WAY
             //USE MEDIAPLAYER CLASS TO GET DURATION, lavede det med David fredag d.29, se i discord
 
-            //use ternary operator to let me pass a null value to the createSong method.
+            //use ternary operator to let me pass a null value to the createSong method. sindsygt cool if/else statement alternativ
             Integer genreId = (selectedGenre != null) ? selectedGenre.getId() : null;
+
             Song newSong = new Song(-1, title, artist, genreId, newFilePath, duration);
-
             songModel.createSong(newSong);
-
     }
 
     public void btnHandleFileChooser(ActionEvent actionEvent) {
         //create file chooser object, with filters for relevant file types.
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select audio file");
-        //MAYBE MAKE A INITIAL DIRECTORY (DESKTOP?)
+        //TODO MAYBE MAKE A INITIAL DIRECTORY (DESKTOP?)
         //fileChooser.setInitialDirectory
 
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3"));
@@ -151,7 +159,8 @@ public class NewSongController implements Initializable {
         genreDataChanged.addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 genreModel.refreshGenre();
-                genreDataChanged.set(false); // Reset the flag
+                // Reset the flag after refreshing
+                genreDataChanged.set(false);
             }
         });
 
